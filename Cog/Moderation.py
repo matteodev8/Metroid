24df62a2df3ecf9eabd0f0c9d506906531d873b5
matteodev8 <matteo2007schweini@gmail.com>
2021-08-08 21:55:55 +0200

    🔨 Ban/Kick commands added

--- Cog/Moderation.py
+++ Cog/Moderation.py
@@ -7,18 +7,31 @@ red = 0xff0000
 yellow = 0xfff200
 blue = 0x0055ff
 
-class Moderation(commands.Cog): # WORK IN PROGRESS
+class Moderation(commands.Cog): 
     def __init__(self, client):
         self.client = client       
 
     @commands.command
     async def ban(self, ctx, member : discord.Member, *, reason=None):
-        channel = discord.Embed(title=f"{member.name} has been banned by {ctx.author.name}", description=f"Reason: {reason}", color=red) #This will send the message to the channel
-        user = discord.Embed(title=f"You have been banned by {ctx.author.name} in {ctx.guild.name}", desc)
+        channel = discord.Embed(title=f"{member.name} has been banned by {ctx.author.name}", description=f"Reason: {reason}", color=red) 
+        user = discord.Embed(title=f"You have been banned by {ctx.author.name} in {ctx.guild.name}", description=f"Reason: {reason}", color=red) 
+        try:
+            await member.send(embed=user)
+        except:
+            await ctx.send("I couldn't DM the user. I will still continue to ban.")
+        await ctx.send(embed=channel)
+        await member.ban(reason = reason) 
 
-        
+    @commands.comamnd
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        channel = discord.Embed(title=f"{member.name} has been kicked by {ctx.author.name}", description=f"Reason: {reason}", color=red) #This will send the message to the channel
+        user = discord.Embed(title=f"You have been kicked by {ctx.author.name} in {ctx.guild.name}", description=f"Reason: {reason}", color=red) #This will send the message to the user
+        try:
+            await member.send(embed=user)
+        except:
+            await ctx.send("I couldn't DM the user. I will still continue to kick") 
+        await ctx.send(embed=channel)
+        await member.kick(reason = reason)
 
-
-
-#def setup(client):
-#    client.add_cog(Moderation(client))       
+def setup(client):
+    client.add_cog(Moderation(client))       
